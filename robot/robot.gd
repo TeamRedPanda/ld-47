af3f05c46112b9fbaa7dae1a80c674b31a85dcbd
Milena <milenaferreira294@hotmail.com>
2020-10-05 11:15:53 -0300

    Add shake

--- robot/robot.gd
+++ robot/robot.gd
@@ -14,8 +14,15 @@ var level
 func move(steps: int):
 	for _i in range(steps):
 		var final_pos := position + _look_direction * _move_distance
+		var shake_direction := Vector2(_look_direction.y, _look_direction.x)
+		var shake_pos1 := position + shake_direction * 10
+		var shake_pos2 := position - shake_direction * 10
 
 		if not level.move(position, _look_direction):
+			var initial_pos := position
+			
+			yield(shake(initial_pos, shake_pos1), "completed")
+			yield(shake(initial_pos, shake_pos2), "completed")
 			continue
 
 		#warning-ignore:RETURN_VALUE_DISCARDED
@@ -39,3 +46,12 @@ func turn(steps: int):
 	yield(_tween, "tween_completed")
 
 	emit_signal("command_executed")
+	
+func shake(initial_pos, shake_pos):
+	_tween.interpolate_property(self, "position", initial_pos, shake_pos, 0.6/6)
+	_tween.start()
+	yield(_tween, "tween_completed")
+	
+	_tween.interpolate_property(self, "position", shake_pos, initial_pos, 0.6/6)
+	_tween.start()
+	yield(_tween, "tween_completed")
