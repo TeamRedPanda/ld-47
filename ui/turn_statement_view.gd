a561423b75ddeaa715b4a54571db2941f4fea37a
Milena <milenaferreira294@hotmail.com>
2020-10-05 14:28:08 -0300

    Fix negative number

--- ui/turn_statement_view.gd
+++ ui/turn_statement_view.gd
@@ -2,6 +2,6 @@ extends Label
 
 func show_statement(stmt: TurnStatement):
 	if stmt._amount < 0:
-		text = "Turn counterclock %s times" %stmt._amount
+		text = "Turn counterclock %s times" %-stmt._amount
 	else:
 		text = "Turn clockwise %s times" %stmt._amount		
