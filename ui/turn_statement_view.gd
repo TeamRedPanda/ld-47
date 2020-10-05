5b253ba782f78a20aedf2a5433383a53c7fc0823
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2020-10-05 15:40:24 -0300

    Add Roboto font to things

--- ui/turn_statement_view.gd
+++ ui/turn_statement_view.gd
@@ -4,4 +4,4 @@ func show_statement(stmt: TurnStatement):
 	if stmt._amount < 0:
 		text = "Turn counterclock %s times" %-stmt._amount
 	else:
-		text = "Turn clockwise %s times" %stmt._amount		
+		text = "Turn clockwise %s times" %stmt._amount
