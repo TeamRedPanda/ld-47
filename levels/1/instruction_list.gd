66f3367231659c5d31a59961c0a7292904efb483
Glauber Guimaraes <glauber-guimaraes@users.noreply.github.com>
2020-10-07 11:42:18 -0300

    Whitespace fixes

--- levels/1/instruction_list.gd
+++ levels/1/instruction_list.gd
@@ -3,12 +3,12 @@ extends VBoxContainer
 
 var code: Code = null
 
-export var move_stmt_view : PackedScene 
+export var move_stmt_view : PackedScene
 export var turn_stmt_view : PackedScene
 
 func set_code(_code: Code):
 	code = _code
-	
+
 	for stmt in code.statements:
 		if stmt is MoveStatement:
 			var view = move_stmt_view.instance()
--- levels/1/item_list.gd
+++ levels/1/item_list.gd
@@ -4,7 +4,7 @@ var code: Code = null
 
 func set_code(_code: Code):
 	code = _code
-	
+
 	for stmt in code.statements:
 		print("o.o")
-	
+
